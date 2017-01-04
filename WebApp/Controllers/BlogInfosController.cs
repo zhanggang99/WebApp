288c6c5c983ade20fea22e88e4ec90cbd0400d0d
'zhanggang99' <'zhanggang@outlook.com'>
2017-01-04 14:21:02 +0800

    完善webapi

--- WebApp/Controllers/BlogInfosController.cs
+++ WebApp/Controllers/BlogInfosController.cs
@@ -10,24 +10,32 @@ namespace WebApp.Controllers
 {
     public class BlogInfosController : ApiController
     {
-        BlogInfo[] bloginfos=new BlogInfo[]
+        BlogInfo[] bloginfos = new BlogInfo[]
         {
             new BlogInfo {BlogId=1,Title="我的第一个博客",Content="我的第一个博客内容",CreatedTime=System.DateTime.Now},
-            new BlogInfo {BlogId=1,Title="我的第二个博客",Content="我的第二个博客内容",CreatedTime=System.DateTime.Now},
-            new BlogInfo {BlogId=1,Title="我的第三个博客",Content="我的第三个博客内容",CreatedTime=System.DateTime.Now},
-            new BlogInfo {BlogId=1,Title="我的第四个博客",Content="我的第四个博客内容",CreatedTime=System.DateTime.Now}
-        }
+            new BlogInfo {BlogId=2,Title="我的第二个博客",Content="我的第二个博客内容",CreatedTime=System.DateTime.Now},
+            new BlogInfo {BlogId=3,Title="我的第三个博客",Content="我的第三个博客内容",CreatedTime=System.DateTime.Now},
+            new BlogInfo {BlogId=5,Title="我的第四个博客",Content="我的第四个博客内容",CreatedTime=System.DateTime.Now}
+        };
         public IEnumerable<BlogInfo> GetAllBlogInfos()
         {
             return bloginfos;
         }
 
-        public IHttpActionResult GetBlogInfo(long blogid)
+        public IHttpActionResult GetBlogInfo(long id)
         {
-            var bloginfo = bloginfos.FirstOrDefault((p)=>p.BlogId==blogid);
+            var bloginfo = bloginfos.FirstOrDefault((p)=>p.BlogId==id);
             if (bloginfo == null)
                 return NotFound();
             return Ok(bloginfo);
         }
+
+        //public IEnumerable<Product> GetProductsByCategory(string category)
+        //{
+        //    return products.Where(
+        //        (p) => string.Equals(p.Category, category,
+        //            StringComparison.OrdinalIgnoreCase));
+        //}
+
     }
 }
