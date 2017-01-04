5309e155fc8b77c712fc3d5d82fe161ed6cbff73
'zhanggang99' <'zhanggang@outlook.com'>
2017-01-04 13:40:09 +0800

    原始提交

---
+++ WebApp/Controllers/BlogInfosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BlogInfosController : ApiController
+    {
+        BlogInfo[] bloginfos=new BlogInfo[]
+        {
+            new BlogInfo {BlogId=1,Title="我的第一个博客",Content="我的第一个博客内容",CreatedTime=System.DateTime.Now},
+            new BlogInfo {BlogId=1,Title="我的第二个博客",Content="我的第二个博客内容",CreatedTime=System.DateTime.Now},
+            new BlogInfo {BlogId=1,Title="我的第三个博客",Content="我的第三个博客内容",CreatedTime=System.DateTime.Now},
+            new BlogInfo {BlogId=1,Title="我的第四个博客",Content="我的第四个博客内容",CreatedTime=System.DateTime.Now}
+        }
+        public IEnumerable<BlogInfo> GetAllBlogInfos()
+        {
+            return bloginfos;
+        }
+
+        public IHttpActionResult GetBlogInfo(long blogid)
+        {
+            var bloginfo = bloginfos.FirstOrDefault((p)=>p.BlogId==blogid);
+            if (bloginfo == null)
+                return NotFound();
+            return Ok(bloginfo);
+        }
+    }
+}
---
+++ WebApp/Models/BlogInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class BlogInfo
+    {
+        public long BlogId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+    }
+}
